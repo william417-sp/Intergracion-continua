845228ff10e20cfcd83b1f6b4347a680de50096d
William Rosado <wrosa2mojica@gmail.com>
2025-06-14 14:52:03 -0400

    wip

--- test/app.test.js
+++ test/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should return HTML', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toContain('Bootcamp de GitHub Action en Código Facilito');
+    expect(res.text).toContain('Bootcamp de GitHub Action en Código Facilito2');
   });
 });
