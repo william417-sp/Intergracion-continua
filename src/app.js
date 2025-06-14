8f26e41a0aff91ba777adf7c2fa7ef258cda3d97
William Rosado <wrosa2mojica@gmail.com>
2025-06-14 18:11:41 -0400

    deploy

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
+  res.send('<h1>Bootcamp de GitHub Action en Código Facilito</h1>');
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
